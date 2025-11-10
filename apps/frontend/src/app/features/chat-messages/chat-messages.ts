4a641f37d39bb5af6e3edb2dc7171213c171e140
Antoine Hésèque <hesequeantoine@gmail.com>
2025-11-10 20:40:36 +0100

    feat: scroll to botton on received message

--- apps/frontend/src/app/features/chat-messages/chat-messages.ts
+++ apps/frontend/src/app/features/chat-messages/chat-messages.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, effect, viewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatMessage } from '../../core/models/chat';
 
@@ -10,4 +10,23 @@ import { ChatMessage } from '../../core/models/chat';
 export class ChatMessagesComponent {
   messages = input<ChatMessage[]>([]);
   isWaitingAnswer = input<boolean>(false);
+
+  private scrollContainer =
+    viewChild<ElementRef<HTMLDivElement>>('scrollContainer');
+
+  constructor() {
+    effect(() => {
+      this.messages();
+      this.scrollToBottom();
+    });
+  }
+
+  private scrollToBottom(): void {
+    setTimeout(() => {
+      const container = this.scrollContainer()?.nativeElement;
+      if (container) {
+        container.scrollTop = container.scrollHeight;
+      }
+    }, 0);
+  }
 }
